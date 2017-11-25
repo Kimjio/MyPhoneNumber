ead9f382667ec96f5ee209f6e3bcd6b5e05d13e2
Kim Ji O <kimjioh0927@gmail.com>
2017-11-25 16:33:55 +0900

    SDK Update

--- app/src/main/java/com/kimjio/sample/myphonenumber/MainActivity.java
+++ app/src/main/java/com/kimjio/sample/myphonenumber/MainActivity.java
@@ -1,29 +1,98 @@
 package com.kimjio.sample.myphonenumber;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static int PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
+        textView = findViewById(R.id.textViewPhone);
 
-        String myPhoneNumber = getPhoneNumber();
-        TextView textView = (TextView) findViewById(R.id.textViewPhone);
-        if(myPhoneNumber == null) {
+        if (getPhoneNumber() == null) {
             textView.setText(android.R.string.unknownName);
         } else {
-            textView.setText(myPhoneNumber);
+            textView.setText(getPhoneNumber());
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermission() {
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
+                // Explain to the user why we need to read the permission.
+                Toast.makeText(this, "Read Phone state", Toast.LENGTH_SHORT).show();
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
+                    PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+            // PERMISSIONS_REQUEST_READ_PHONE_STATE is an
+            // app-defined int constant
+
+        } else {
+            // 다음 부분은 이미 허용일 경우에 해당이 됩니다.
+            //TODO
+            Toast.makeText(this, "이미 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_PHONE_STATE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    //TODO
+
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+                    Toast.makeText(getApplicationContext(), "권한 허가됨", Toast.LENGTH_SHORT).show();
+                    textView.setText(getPhoneNumber());
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "권한 거부됨", Toast.LENGTH_SHORT).show();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+        }
+    }
+
+    @SuppressLint("HardwareIds")
     public String getPhoneNumber() {
-        TelephonyManager mgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        return mgr.getLine1Number();
+        TelephonyManager mgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+        return mgr != null ? mgr.getLine1Number() : null;
     }
 }
