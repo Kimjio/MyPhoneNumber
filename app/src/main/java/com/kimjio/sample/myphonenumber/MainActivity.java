2c8b449d8ea91816047f7ceaacce84765860bd09
Kim Ji O <kimjioh0927@gmail.com>
2017-12-10 15:08:41 +0900

    Add Permission check

--- app/src/main/java/com/kimjio/sample/myphonenumber/MainActivity.java
+++ app/src/main/java/com/kimjio/sample/myphonenumber/MainActivity.java
@@ -18,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
     public final static int PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkPermission();
+
         textView = findViewById(R.id.textViewPhone);
 
         if (getPhoneNumber() == null) {
@@ -33,48 +34,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @TargetApi(Build.VERSION_CODES.M)
-    private void checkPermission() {
-        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
-                // Explain to the user why we need to read the permission.
-                Toast.makeText(this, "Read Phone state", Toast.LENGTH_SHORT).show();
-            }
-
-            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
-                    PERMISSIONS_REQUEST_READ_PHONE_STATE);
-
-            // PERMISSIONS_REQUEST_READ_PHONE_STATE is an
-            // app-defined int constant
-
-        } else {
-            // 다음 부분은 이미 허용일 경우에 해당이 됩니다.
-            //TODO
-            Toast.makeText(this, "이미 허용", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case PERMISSIONS_REQUEST_READ_PHONE_STATE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    //TODO
-
-                    // permission was granted, yay! do the
-                    // calendar task you need to do.
-                    Toast.makeText(getApplicationContext(), "권한 허가됨", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Granted", Toast.LENGTH_SHORT).show();
                     textView.setText(getPhoneNumber());
                 } else {
-
-                    Toast.makeText(getApplicationContext(), "권한 거부됨", Toast.LENGTH_SHORT).show();
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(), "Denied", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
@@ -83,16 +51,17 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("HardwareIds")
     public String getPhoneNumber() {
         TelephonyManager mgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)) {
+                Toast.makeText(this, "Read Phone state", Toast.LENGTH_SHORT).show();
+            }
+
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
+                    PERMISSIONS_REQUEST_READ_PHONE_STATE);
             return null;
+        } else {
+            Toast.makeText(this, "Already", Toast.LENGTH_SHORT).show();
+            return mgr != null ? mgr.getLine1Number() : null;
         }
-        return mgr != null ? mgr.getLine1Number() : null;
     }
 }
